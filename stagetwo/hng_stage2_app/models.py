fce9b59e7ee373b386cbfec36afb231399b73911
Opeyemi-daniel <opeyemi.daniel@stu.cu.edu.ng>
2023-09-13 23:27:51 +0100

    name fields alone

--- stagetwo/hng_stage2_app/models.py
+++ stagetwo/hng_stage2_app/models.py
@@ -4,8 +4,6 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=30, unique=True)
-    email = models.CharField(max_length=20)
-    age = models.CharField(max_length=3)
 
     def __str__(self):
         return self.name
---
+++ test.py
@@ -0,0 +1,54 @@
+import requests
+import json
+
+# Define the base URL of your Spring Boot application
+base_url = 'http://authur.pythonanywhere.com/api/user'
+
+# Function to send a POST request to create a person
+def create_person(name):
+    data = {'name': name}
+    response = requests.post(f'{base_url}', json=data)  # Append the create endpoint
+    return response
+
+# Function to send a GET request to read a person by ID
+def read_person(name):
+    response = requests.get(f'{base_url}/{name}')  # Append the ID as a path variable
+    return response
+
+# Function to send a PUT request to update a person by ID
+def update_person(name, new_name):
+    data = {'name': new_name}
+    response = requests.put(f'{base_url}/{name}', json=data)  # Append the ID as a path variable
+    return response
+
+# Function to send a DELETE request to delete a person by ID
+def delete_person(name):
+    response = requests.delete(f'{base_url}/{name}')  # Append the ID as a path variable
+    return response
+
+# Test the create endpoint
+create_response = create_person('John Doe')
+print('Create Person Response:')
+print(create_response.status_code)
+print(create_response.text)
+print('\n')
+
+# Test the read endpoint
+read_response = read_person(1)
+print('Read Person Response:')
+print(read_response.status_code)
+print(read_response.text)
+print('\n')
+
+# Test the update endpoint
+update_response = update_person(1, 'Jane Doe')
+print('Update Person Response:')
+print(update_response.status_code)
+print(update_response.text)
+print('\n')
+
+# Test the delete endpoint
+delete_response = delete_person(1)
+print('Delete Person Response:')
+print(delete_response.status_code)
+print(delete_response.text)
