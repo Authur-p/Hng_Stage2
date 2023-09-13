e1382785fb7cf837023dca0daed3a2caacb36947
Opeyemi-daniel <opeyemi.daniel@stu.cu.edu.ng>
2023-09-13 15:53:49 +0100

    Charfield only

--- stagetwo/hng_stage2_app/models.py
+++ stagetwo/hng_stage2_app/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    name = models.CharField(max_length=30)
-    email = models.EmailField()
-    age = models.IntegerField()
+    name = models.CharField(max_length=30, unique=True)
+    email = models.CharField(max_length=20)
+    age = models.CharField(max_length=3)
 
     def __str__(self):
         return self.name
